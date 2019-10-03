6d3e300da4e7a88620173a286e08c665b80b3b3f
Matt Neuburg <matt@tidbits.com>
2019-10-03 11:41:24 -0700

    added toggle

--- LargeTitleTest/ViewController.swift
+++ LargeTitleTest/ViewController.swift
@@ -4,7 +4,16 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
-
+    
+    var currentMode = UIUserInterfaceStyle.light
+    
+    @IBAction func doToggle(_ sender: Any) {
+        self.currentMode = (self.currentMode == .light) ? .dark : .light
+        self.navigationController?.overrideUserInterfaceStyle = self.currentMode
+    }
+    @IBAction func doAuto(_ sender: Any) {
+        self.navigationController?.overrideUserInterfaceStyle = .unspecified
+    }
+    
 }
 
